be800fa97f8f306b13a23428b396e5ff8fb8e713
Gabryel Deusino <gdeusinosousafreitas@gmail.com>
2025-07-22 17:19:07 -0300

    Aplicação finalizada

--- src/main/java/br/com/gabryel/todolist/entity/Todo.java
+++ src/main/java/br/com/gabryel/todolist/entity/Todo.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "todos")
@@ -12,12 +13,22 @@ public class Todo {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NotBlank
 	private String nome;
+	@NotBlank
 	private String descricao;
 	private boolean realizado;
 	private int prioridade;
 	
-//	Getters e Setters
+	public Todo(String nome, String descricao, boolean realizado, int prioridade) {
+		super();
+		this.nome = nome;
+		this.descricao = descricao;
+		this.realizado = realizado;
+		this.prioridade = prioridade;
+	}
+	
+	//	Getters e Setters
 	public Long getId() {
 		return id;
 	}
